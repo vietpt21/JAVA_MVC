ea925c534c9ac1e169f0714b28a894e087e0aa77
vietpt21 <vietvpvbhp@gmail.com>
2023-09-17 07:45:06 -0700

    fdgdfgdfg

---
+++ shopbaby/src/main/java/vietpt/entity/NhanVien.java
@@ -0,0 +1,11 @@
+package vietpt.entity;
+
+public class NhanVien {
+    String name ;
+    String address;
+    String age ;
+    public  NhanVien(){    }
+    public  void getInfomation(){
+        System.out.println("Test Thong Bao ");
+    }
+}
